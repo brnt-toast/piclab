47983736121021dcc7ee93b34f1635744060579e
brnt-toast <yoyo.beerme@gmail.com>
2022-04-28 19:58:09 -0400

    init: Nav

---
+++ components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/Link'
+
+import logo from '../assets/logo.svg'
+
+export default function Nav() {
+  return (
+    <div className='flex w-5/6 justify-between m-auto'>
+        <div className="flex ">
+            <Image src={logo} alt="pic lab log" width={55} height={25} />
+            <div className="container">picslab</div>
+        </div>
+        <div className="links w-4/12 flex justify-between">
+            <Link href='#'>Download</Link>
+            <Link href='#'>Docs</Link>
+            <Link href='#'>Login</Link>
+            <button className='bg-fuchsia-800 p-2' >
+                <Link href='#'>Request Beta Access</Link>
+            </button>
+
+        </div>
+
+    </div>
+  )
+}
