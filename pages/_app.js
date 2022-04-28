f1469ae97c25eb3748e7f8d3710586c2bee20458
brnt-toast <yoyo.beerme@gmail.com>
2022-04-28 13:02:58 -0400

    working: copy and paste from docs

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,22 @@ import { linkResolver, repositoryName } from '../prismicio'
 
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>
+            {children}
+          </a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  )
 }
 
-export default MyApp
