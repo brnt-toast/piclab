4a7d105872ad4a962dcad412a6d4e533283d0b6b
brnt-toast <yoyo.beerme@gmail.com>
2022-04-28 11:58:38 -0400

    init

---
+++ .slicemachine/assets/slices/Email/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Email';
+
+export default {
+  title: 'slices/Email'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"email","items":[],"primary":{"title":[{"type":"heading1","text":"Aggregate open-source paradigms","spans":[]}],"description":[{"type":"paragraph","text":"Culpa irure consectetur aute est nisi aliqua.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,13 @@
+/* _app.js == root in nextj.js
+
+*/
+
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react' // utilities and settings
+import { PrismicPreview } from '@prismicio/next' // enable preview
+import { linkResolver, repositoryName } from '../prismicio'
+
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
