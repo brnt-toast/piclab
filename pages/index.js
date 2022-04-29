b9a982602a21732a64f3e03310184287993a0083
brnt-toast <yoyo.beerme@gmail.com>
2022-04-29 13:47:36 -0400

    add: display grid to index.js

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,19 @@ import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div className=''>
+    <div>
       <Head>
         <title>PicLab</title>
         <meta name="description" content="Your Picture Laboratory" />
         <link rel="icon" href="/logo.svg" />
       </Head>
 
-      <main>
+      <main className=''>
         <Nav />
-        <Hero />
-        
-        
+        <div className='grid flex-col grid-cols-2'>
+          <Hero />
+
+        </div>
       </main>
     </div>
   )
