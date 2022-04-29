fe4cc156cd691e168f203c69e77810c5139a0147
brnt-toast <yoyo.beerme@gmail.com>
2022-04-29 13:58:51 -0400

    add: indexjs -- refactored width and flexbox

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex justify-center'>
       <Head>
         <title>PicLab</title>
         <meta name="description" content="Your Picture Laboratory" />
         <link rel="icon" href="/logo.svg" />
       </Head>
 
-      <main className=''>
+      <main className=' w-11/12'>
         <Nav />
         <div className='grid flex-col grid-cols-2'>
           <Hero />
