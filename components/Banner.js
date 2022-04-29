9a43b0aa89955ede8c891fcf389d62bf4d800734
brnt-toast <yoyo.beerme@gmail.com>
2022-04-29 12:38:17 -0400

    update: component doc

---
+++ components/Banner.js
@@ -0,0 +1,10 @@
+/* BANNER.JS -- FIGMA : FEATURE BANNER
+Props : image, title, text, button, background color
+*/
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div>Banner</div>
+  )
+}
