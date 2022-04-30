ed5314d8757be6e2b5bb0df419cd422924d87a95
brnt-toast <yoyo.beerme@gmail.com>
2022-04-30 19:01:37 -0400

    init: FeatureRack.js

---
+++ components/FeatureRack.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import FeatureBox from './FeatureBox'
+
+export default function FeatureRack() {
+  return (
+    <div className="inline-grid grid-cols-2">
+             <FeatureBox
+              icon="fa-solid fa-gear"
+              title="Ultra-Fast"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <FeatureBox
+              icon="fa-solid fa-gear"
+              title="Ultra-Fast"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <FeatureBox
+              icon="fa-solid fa-gear"
+              title="Ultra-Fast"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <FeatureBox
+              icon="fa-solid fa-gear"
+              title="Ultra-Fast"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <FeatureBox
+              icon="fa-solid fa-gear"
+              title="Ultra-Fast"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <FeatureBox
+              icon="fa-solid fa-gear"
+              title="Ultra-Fast"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+    </div>
+  )
+}
