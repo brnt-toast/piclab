2003fc712dec3b36c3694dada4f48e5bc1d5a5ae
brnt-toast <yoyo.beerme@gmail.com>
2022-04-30 16:00:31 -0400

    added: custom fontSize-ing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontSize:{
+      'base': '1rem',   // 16px
+      'lg': '1.125rem', // 18px
+      'xl': '1.25rem',  // 20px
+      '2xl': '1.5rem',  // 24px
+      '3xl': '1.875rem',// 30px
+      '4xl': '2rem',    // 32px
+      '5xl': '3rem',    // 48px
+      '6xl': '4.2rem',  // 67px
+      '7xl': '5.4rem'   // 86px
+    } 
   },
   plugins: [],
 }
