fd00c68c7cc5217d5c1e88dfb298ba809347a635
brnt-toast <yoyo.beerme@gmail.com>
2022-04-30 12:35:25 -0400

    init: Email.js; del: EmailComponents.js

---
+++ components/Email.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Email() {
+  return (
+    <div className='w-1/6'>
+        <button className='w-24 h-24 rounded'>
+            <i className="fa-regular fa-lock-keyhole"></i>
+        </button>
+        <input type="Enter your email for beta access..." />
+    </div>
+  )
+}
