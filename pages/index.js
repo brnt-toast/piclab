5532845ab98975c268a089c8d5341bc5ab1d3f63
brnt-toast <yoyo.beerme@gmail.com>
2022-05-01 14:48:25 -0400

    refactor: index.js

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,12 @@ import Script from 'next/script'
 import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 import Title from '../components/Title'
-import Banner from '../components/Banner'
+import BannerSection from '../components/BannerSection'
 import FeatureRack from '../components/FeatureRack'
+import FeatureSection from '../components/FeatureSection'
 import Email from '../components/Email'
 
-import img from '/public/Image-Group-1.png'
-import colab from '/public/colab.png'
-import compose from '/public/compose.png'
-import organize from '/public/organize.png'
-import powerful from '/public/powerful.png'
 import nextGen from '/public/next-gen-feats.png'
-
 
 export default function Home() {
   return (
@@ -46,39 +41,8 @@ export default function Home() {
             alt="features image"
           />
           <FeatureRack />
-          <div className='col-span-full'>
-            <Title
-              size="text-6xl"
-              title="One unified toolset."
-              content="Finally, an image management app that devs, designers, and marketing teams can agree on.."
-            />
-            <Image
-              src={img}
-              alt="image showcase"
-              className='block col-span-full'
-            />
-          </div>
-          <Banner
-            src={colab}
-            alt='colob phot'
-            title='Collaborate.'
-            content='Build image libraries, called workspaces, to collaborate across teams and organizations. Tempor, viverra in fames gravida. Magna placerat sodales id fermentum ultrices et commodo volutpat viverra. Nibh mauris, adipiscing suspendisse eros, quis id magna sed pellentesque sed.'
-          />
-          <Banner src={organize}
-            alt="organize screen shot"
-            title="Organize."
-            content="Group images by tags, or let our AI do it for you, and grab a cup of coffee instead. Aliquet non dui amet integer sit rhoncus quis pharetra. Pellentesque sed elementum fusce auctor adipiscing. Adipiscing arcu pulvinar nisl sit dapibus."
-          />
-          <Banner src={compose}
-            alt="composition screen shot"
-            title="Compose."
-            content="Prep your images for any type of project, with social media presets baked in. Nunc facilisi viverra in nisl eget egestas cum elementum netus. Aliquet non dui amet integer sit rhoncus quis pharetra."
-          />
-          <Banner src={powerful}
-            alt="lion"
-            title="Powerful."
-            content="Under the hood, PicLab is powered by imgix, the most robust image processor on the web. Use our ultra-fast CDN to host your images, optimize, or download your entire library, all backed by technology that serves billions of images, daily. "
-          />
+          <FeatureSection />
+          <BannerSection />
           <Title
             size="text-6xl"
             title="Ready to give PicLab a try?"
