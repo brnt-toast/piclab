50e2c1ab716346a3c04df50e3b4a2a27f608ea57
brnt-toast <yoyo.beerme@gmail.com>
2022-05-01 16:16:24 -0400

    refactor

--- pages/index.js
+++ pages/index.js
@@ -17,46 +17,44 @@ import img from '/public/Image-Group-1.png'
 export default function Home() {
   return (
     <div>
-    <Head>
-        <title>PicLab</title>
-        <meta name="description" content="Your Picture Laboratory" />
-        <link rel="icon" href="/logo.svg" />
-        <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
-        <link rel="stylesheet" href="https://cloud.typography.com/6929016/7418832/css/fonts.css" />
-    </Head>
+      <Head>
+          <title>PicLab</title>
+          <meta name="description" content="Your Picture Laboratory" />
+          <link rel="icon" href="/logo.svg" />
+          <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+          <link rel="stylesheet" href="https://cloud.typography.com/6929016/7418832/css/fonts.css" />
+      </Head>
       
       <Script src="https://kit.fontawesome.com/117bcfe378.js" />
       
-        <Nav />
-        <Hero />
-        <FeatureSection 
+      <Nav />
+      <Hero />
+      <FeatureSection 
         size = "text-6xl"
         title="Next-gen features."
         content="A powerful set of tools designed to work in any situation."
         img={nextGen}
         altTag = "features image"
         classes = '' 
-        >
-            <FeatureRack />
-          </FeatureSection>
-          <FeatureSection 
+      >
+          <FeatureRack />
+        </FeatureSection>
+        <FeatureSection 
           size = "text-6xl"
           title="One unified toolset." 
           content="Finally, an image management app that devs, designers, and marketing teams can agree on.." 
           img={img}
           altTag="image showcase"
           classes="'block col-span-full'"
-          />
-          <BannerSection /> 
-          <Title
+        />
+        <BannerSection /> 
+        <Title
           size="text-6xl"
           title="Ready to give PicLab a try?"
           content="Become an early adpoter by joining our beta test program and invite friends and collegues."
-          />
-          <Email />
-
-        </div>
-        
-        )
-      }
+        />
+        <Email />
+    </div>
+  )
+}
       
