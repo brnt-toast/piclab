e8a471f9deac2b92ca63988e61eaf5dbec50c357
brnt-toast <yoyo.beerme@gmail.com>
2022-05-01 16:14:09 -0400

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Script from 'next/script'
+
 
 
 import Nav from '../components/Nav'
@@ -12,45 +12,51 @@ import FeatureSection from '../components/FeatureSection'
 import Email from '../components/Email'
 
 import nextGen from '/public/next-gen-feats.png'
+import img from '/public/Image-Group-1.png'
 
 export default function Home() {
   return (
-    <div className='flex justify-center'>
-      <Head>
+    <div>
+    <Head>
         <title>PicLab</title>
         <meta name="description" content="Your Picture Laboratory" />
         <link rel="icon" href="/logo.svg" />
         <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
         <link rel="stylesheet" href="https://cloud.typography.com/6929016/7418832/css/fonts.css" />
-      </Head>
+    </Head>
+      
       <Script src="https://kit.fontawesome.com/117bcfe378.js" />
-
-      <main className=' w-11/12'>
+      
         <Nav />
-        <div className='grid flex-col grid-cols-2'>
-          <Hero />
-          <div className='flex-col col-span-full'>
-            <Title
-              size="text-6xl"
-              title="Next-gen features."
-              content="A powerful set of tools designed to work in any situation."
-            />
-          </div>
-          <Image
-            src={nextGen}
-            alt="features image"
+        <Hero />
+        <FeatureSection 
+        size = "text-6xl"
+        title="Next-gen features."
+        content="A powerful set of tools designed to work in any situation."
+        img={nextGen}
+        altTag = "features image"
+        classes = '' 
+        >
+            <FeatureRack />
+          </FeatureSection>
+          <FeatureSection 
+          size = "text-6xl"
+          title="One unified toolset." 
+          content="Finally, an image management app that devs, designers, and marketing teams can agree on.." 
+          img={img}
+          altTag="image showcase"
+          classes="'block col-span-full'"
           />
-          <FeatureRack />
-          <FeatureSection />
-          <BannerSection />
+          <BannerSection /> 
           <Title
-            size="text-6xl"
-            title="Ready to give PicLab a try?"
-            content="Become an early adpoter by joining our beta test program and invite friends and collegues."
+          size="text-6xl"
+          title="Ready to give PicLab a try?"
+          content="Become an early adpoter by joining our beta test program and invite friends and collegues."
           />
           <Email />
+
         </div>
-      </main>
-    </div>
-  )
-}
+        
+        )
+      }
+      
