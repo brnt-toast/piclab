35d272f2b4ae4b4d49bf5056bc1e3e713e866cb1
brnt-toast <yoyo.beerme@gmail.com>
2022-05-03 10:06:49 -0400

    init: Button.js

---
+++ components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button({children}) {
+  return (
+    <button className='bg-_purple py-2 px-5 rounded-md'>
+        {children}
+    </button>
+  )
+}
