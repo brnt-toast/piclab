7b2357477a21457b10f90d070cf25af3eb57cb35
brnt-toast <yoyo.beerme@gmail.com>
2022-05-05 16:22:23 -0400

    add: preload

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
-        <link rel="stylesheet" href="https://cloud.typography.com/6929016/7418832/css/fonts.css" />
+        <link rel='preload' rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+        <link rel='preload' rel="stylesheet" href="https://cloud.typography.com/6929016/7418832/css/fonts.css" />
       </Head>
       <body className='bg-black text-white'>
         <Main />
